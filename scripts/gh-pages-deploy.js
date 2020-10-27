b976f3b659f810ec54b4f122302345c70859bff6
ozemen <o.zemen@gmail.com>
2020-10-27 20:56:04 +0100

    script fix

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -9,6 +9,10 @@ const fs = require("fs");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
+    // Create CNAME file with the custom domain name
+    const strm = fs.createWriteStream(folderName + '/CNAME');
+    strm.write('client.doupak.cz');
+    strm.end();
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
@@ -17,6 +21,8 @@ const fs = require("fs");
     await execa("git", ["checkout", "-f", "main"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
+
+
   } catch (e) {
     // eslint-disable-next-line no-console
     console.log(e.message);
